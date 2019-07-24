2217cb99863bd572bafdf509a9632ca4a089a2f5
shafi <abdullahalshafi1212@gmail.com>
2019-07-24 18:31:38 +0600

    sidebar

--- src/app/main/dashboard/common/dashboard-sidenav/dashboard-sidenav.component.ts
+++ src/app/main/dashboard/common/dashboard-sidenav/dashboard-sidenav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Dashboard } from '../../../navigation/dashboard';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-sidenav',
@@ -13,17 +15,24 @@ export class DashboardSidenavComponent implements OnInit {
 
   @Output() closeNavBar = new EventEmitter<any>();
 
-  constructor() { }
+  constructor(
+   private media: MediaMatcher,
+   private router: Router
+  ) { 
 
-  ngOnInit() {
-
-    this.mediaQuery = window.matchMedia('(max-width: 900px)');
-
+    this.mediaQuery = media.matchMedia('(max-width: 900px)');
     this.dashboard = Dashboard;
+  
   }
 
-  closeSideNav(): void{
-    this.closeNavBar.emit({closeSideNav: true});
+  ngOnInit() {}
+
+  handleSideNav(url): void {
+
+    if (this.mediaQuery) {
+      this.closeNavBar.emit({closeSideNav: true});
+    }
+    url ? this.router.navigateByUrl(url) : this.router.navigateByUrl('/dashboard');
   }
 
   get self() { return this; }
@@ -33,3 +42,4 @@ export class DashboardSidenavComponent implements OnInit {
   }
 
 }
+
